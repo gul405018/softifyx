4b90e09bfd94637f3304b1f4258c6991c9b2d3eb
Naimat Ullah <gul405018@gmail.com>
2026-04-11 13:58:45 +0500

    a

--- assets/js/app.js
+++ assets/js/app.js
@@ -469,8 +469,15 @@
             const overlay = document.getElementById('modalOverlay2');
             const container = document.getElementById('modalContainer2');
             
-            if (isWide) container.classList.add('modal-wide');
-            else container.classList.remove('modal-wide');
+            if (isWide) {
+                container.classList.add('modal-wide');
+                container.style.width = '1250px';
+                container.style.maxWidth = '1250px';
+            } else {
+                container.classList.remove('modal-wide');
+                container.style.width = '';
+                container.style.maxWidth = '';
+            }
             
             const dataModuleTag = moduleKey || title.text;
             
