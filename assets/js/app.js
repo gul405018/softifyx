66e8cf3243e31d7b05c569f4e319131e9f9a4cd9
Naimat Ullah <gul405018@gmail.com>
2026-04-17 15:10:58 +0500

    a

--- assets/js/app.js
+++ assets/js/app.js
@@ -2345,7 +2345,7 @@
                         else if (url.includes('currency.html')) setTimeout(() => initCurrencyView(), 10);
                         else if (url.includes('chart_of_accounts.html')) setTimeout(() => initChartOfAccountsView(), 10);
                         else if (url.includes('employees.html')) setTimeout(() => initEmployeesView(), 10);
-                        else if (url.includes('inventory_coa.html')) setTimeout(() => initInventoryCOAView(), 10);
+                        else if (url.includes('chart_of_inventory.html')) setTimeout(() => initInventoryCOAView(), 10);
                     }
                 } else {
                     openModal({ icon: titleIcon, text: titleText }, 
@@ -2667,7 +2667,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                     let isEmp = (moduleName === "Employees" || (targetUrl && targetUrl.includes('employees.html')));
                     let isBank = (moduleName === "Bank Accounts" || (targetUrl && targetUrl.includes('bank_accounts.html')));
                     let isOB = (moduleName === "Accounts Opening Balances" || (targetUrl && targetUrl.includes('accounts_opening_balances.html')));
-                    let isInv = (moduleName === "Chart Of Inventory" || (targetUrl && targetUrl.includes('inventory_coa.html')));
+                    let isInv = (moduleName === "Chart Of Inventory" || (targetUrl && targetUrl.includes('chart_of_inventory.html')));
                     let initCallback = isCoa ? initChartOfAccountsView : (isCust ? initCustomersView : (isVend ? initVendorsView : (isReg ? initRegionsView : (isEmp ? initEmployeesView : (isBank ? initBankAccountsView : (isOB ? initOpeningBalancesView : (isInv ? initInventoryCOAView : null))))))));
                     window.openModularPopup(targetUrl, 'fa-file-alt', titleText, initCallback, moduleName, (isCoa || isCust || isVend || isReg || isEmp || isBank || isOB || isInv));
                     
@@ -2699,7 +2699,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                     let isEmp = (moduleName === "Employees" || (targetUrl && targetUrl.includes('employees.html')));
                     let isBank = (moduleName === "Bank Accounts" || (targetUrl && targetUrl.includes('bank_accounts.html')));
                     let isOB = (moduleName === "Accounts Opening Balances" || (targetUrl && targetUrl.includes('accounts_opening_balances.html')));
-                    let isInv = (moduleName === "Chart Of Inventory" || (targetUrl && targetUrl.includes('inventory_coa.html')));
+                    let isInv = (moduleName === "Chart Of Inventory" || (targetUrl && targetUrl.includes('chart_of_inventory.html')));
                     let initCallback = isCoa ? initChartOfAccountsView : (isCust ? initCustomersView : (isVend ? initVendorsView : (isReg ? initRegionsView : (isEmp ? initEmployeesView : (isBank ? initBankAccountsView : (isOB ? initOpeningBalancesView : (isInv ? initInventoryCOAView : null))))))));
                     window.openModularPopup(targetUrl, 'fa-file-alt', titleText, initCallback, moduleName, (isCoa || isCust || isVend || isReg || isEmp || isBank || isOB || isInv));
                 });
